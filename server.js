36e653ffef6351019981fc19c9ce59b84528b415
= <=>
2025-04-30 09:56:07 +0200

    ejercicio 2 y 3

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const port = 8000;
 const baseURL = `http://localhost:${port}`;
 const foods = ["Paella", "Arroç al forn", "Arroç al senyoret"];
 let randomNumber;
+let numbersArray = [];
 
 const express = require("express");
 const cors = require("cors");
@@ -14,17 +15,25 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send(`<a href="/foods">El ejercicio 1 está en /foods</a>
     <br>
-    <a href="/foods">El ejercicio 2 está en /minmax</a>
+    <a href="/number">El ejercicio 2 está en /number</a>
     `);
 });
 
 //Ejercicio 1
 app.get("/foods", (req, res) => {
   randomNumber = Math.floor(Math.random() * 3);
-  res.send(`The food for today's lunch is ${foods[randomNumber]}</a>`);
+  res.send(`The food for today's lunch is ${foods[randomNumber]}`);
 });
 
 //Ejercicio 2
+app.post("/number", (req, res) => {
+  const newNumber = req.body.number;
+  numbersArray.push(newNumber);
+  const MAX = Math.max(...numbersArray);
+  const MIN = Math.min(...numbersArray);
+
+  res.send(`Min: ${MIN}   Max: ${MAX}`);
+});
 
 app.listen(port, () => {
   console.log(`Listen on ${baseURL}`);
